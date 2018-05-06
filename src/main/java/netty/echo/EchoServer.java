2291f82b3ea505a9b328057f1e72f2d95c8c0306
nongzhenqin <694159256@qq.com>
2018-05-06 16:58:20 +0800

    echo

--- src/main/java/netty/echo/EchoServer.java
+++ src/main/java/netty/echo/EchoServer.java
@@ -1,13 +1,20 @@
 package netty.echo;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
 
 public class EchoServer {
 
-    private final int port;
+    private int port;
 
-    public EchoServer(int port){
+    public EchoServer(int port) {
         this.port = port;
     }
 
@@ -16,15 +23,35 @@ public class EchoServer {
     }
 
     public void start(){
-        final EchoServerHandler echoServerHandler = new EchoServerHandler();
+        EventLoopGroup eventLoopGroup = new NioEventLoopGroup();
 
-        EventLoopGroup group = new NioEventLoopGroup();
         try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(eventLoopGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .localAddress(new InetSocketAddress(port))
+                    // 添加一个EchoServerHandlerd到子Channel的ChannelPipeline
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            // EchoServerHandler被添加@Shareable，所以我们总是可以使用同一个实例
+                            socketChannel.pipeline().addLast(new EchoServerHandler());
+                        }
+                    });
 
+            // 异步的绑定服务器，调用sync方法阻塞等待直到绑定完成
+            ChannelFuture sync = serverBootstrap.bind().sync();
+            // 获取Channel的closeFuture，并且阻塞当前线程直到它完成
+            sync.channel().closeFuture().sync();
         }catch (Exception e){
             e.printStackTrace();
         }finally {
-            group.shutdownGracefully().sync();
+            try {
+                // 关闭eventLoopGroup释放所有资源
+                eventLoopGroup.shutdownGracefully().sync();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
