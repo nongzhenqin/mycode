0cfc1ec7f00d903122203f0c4332de804e6d2104
nongzhenqin <694159256@qq.com>
2021-05-06 18:35:13 +0800

    更新spring-boot版本

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,20 +1,22 @@
 import org.junit.Assert;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class Test {
 
-//    @org.junit.Test
+    @org.junit.Test
     public void testOptional(){
         Optional<String> optional = Optional.of("A");
         Assert.assertEquals("A", optional.get());
     }
 
-//    @org.junit.Test
+    @org.junit.Test
     public void testArray(){
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
@@ -54,6 +56,12 @@ public class Test {
             nums[i] = re[i];
             System.out.println(nums[i]);
         }
+    }
+
+    @org.junit.Test
+    public void testHashMapSize(){
+        Map map = new HashMap(16);
+        System.out.println(map.size());
     }
 
 }
